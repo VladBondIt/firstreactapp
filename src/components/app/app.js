0456c3cd1fbd8f79f385456a6f1293a720a047a4
Vlad Bond <foolowin@gmail.com>
2020-11-13 19:44:12 +0300

    tbc

---
+++ src/components/app/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import AppHeader from '../app-header/app-header';
+import SearchPanel from '../search-panel/search-panel';
+import PostStatusFilter from '../post-status-filter/post-status-filter';
+import PostList from '../post-list/post-list';
+
+const App = () => {
+    return (
+        <div className="app">
+            <AppHeader />
+            <div className="search-panel d-flex">
+                <SearchPanel />
+                <PostStatusFilter />
+            </div>
+            <PostList />
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import './css/app.css';
+import './css/post-list.css';
+import './css/post-status-filter.css';
+import './css/search-panel.css';
+import './css/app-header.css';
+import './css/post-add-form.css';
+import './css/post-list-item.css';
+import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
 
+
+// Кoмпоненты это функции которые могут возвращать jsx елементы,
+// они должны быть написаны с большой буквы
+
+// const Header = () => {
+//   // Защитаот вредоносных скриптов, реакт тупо записывает его сторокой =)
+//   const scr = `Hello world!!`
+//   return <h2>{scr}</h2>
+// }
+
+// const Field = () => {
+//   const holder = "Enter here";
+//   const styledField = {
+//     width: `300px`
+//   }
+
+//   return <input
+//     style={styledField}
+//     type="text"
+//     placeholder={holder}
+//     autoComplete=""
+//     className="first"
+//     htmlFor=""
+//   />
+// }
+
+// const Button = () => {
+//   const text = `Log in`;
+//   const logged = false;
+//   // const res = () => {
+//   //   return `Log in, please`
+//   // }
+//   // ошибка, объекты нельзяя помещать в переменные для рендера их в приложении
+//   // const p = {}
+//   // const p = <p>Log in</p>
+//   return <button>{logged ? `Enter` : text}</button>
+//   // return <button>{3 + 4}</button>
+// }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <Field />
+//       <Button />
+//     </div>
+//   )
+// }
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
